0a493f3c974dcd8bff0dbf51bf7a59210fc5dc5d
LarsenW <wolflarsen10@gmail.com>
2015-11-02 20:25:43 +0200

    make view - db connection

--- web/src/main/java/com/storehouse/web/controllers/HelloController.java
+++ web/src/main/java/com/storehouse/web/controllers/HelloController.java
@@ -1,18 +1,24 @@
 package com.storehouse.web.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.ModelMap;
+
+import com.storehouse.business.services.UserService;
+import com.storehouse.common.entity.User;
 
 @Controller
 public class HelloController {
+	@Autowired
+	UserService userService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-
-		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", userService.getById(1L).getName());
 		return "hello";
 
 	}
