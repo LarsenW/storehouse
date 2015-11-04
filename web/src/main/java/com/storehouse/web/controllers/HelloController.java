2fa2d455828204a05f1fb6a35d06b55163dc8ab2
LarsenW <wolflarsen10@gmail.com>
2015-11-04 23:43:39 +0200

    add spring-security based on xml  configs

--- web/src/main/java/com/storehouse/web/controllers/HelloController.java
+++ web/src/main/java/com/storehouse/web/controllers/HelloController.java
@@ -1,15 +1,11 @@
 package com.storehouse.web.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.storehouse.business.services.UserService;
-import com.storehouse.common.entity.User;
 
 @Controller
 public class HelloController {
