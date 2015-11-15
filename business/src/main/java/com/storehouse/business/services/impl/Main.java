09db0375f9652574d49970cc617c3188ec6d8e4f
LarsenW <wolflarsen10@gmail.com>
2015-11-15 15:21:33 +0200

    add password hashing by MD5 algorithm

--- business/src/main/java/com/storehouse/business/services/impl/Main.java
+++ business/src/main/java/com/storehouse/business/services/impl/Main.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.storehouse.business.services.UserService;
+import com.storehouse.business.utils.PasswordHelper;
 import com.storehouse.common.entity.Role;
 import com.storehouse.common.entity.User;
 import com.storehouse.common.enums.UserType;
@@ -16,13 +17,17 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("business-context.xml");
 		User u = new User();
-		u.setEmail("user@gmail.com");
-		u.setPassword("user");
+		u.setEmail("grisha@gmail.com");
+
 		Role role = new Role();
 		role.setUserType(UserType.ROLE_USER);
 		u.setUserRoles(new HashSet<Role>(Arrays.asList(role)));
 		UserService s = (UserService) context.getBean("userServiceImpl");
-		 s.persistUser(u);
+		PasswordHelper ph = (PasswordHelper) context.getBean("passwordHelper");
+		String encodedPassword = ph.encode("1111");
+		u.setPassword(encodedPassword);
+
+		s.persistUser(u);
 		// System.out.println(s.getById(3l).getUserRoles());
 		// System.out.println(s.loadUserByUsername("Alfred"));
 	}
---
+++ business/src/main/java/com/storehouse/business/utils/PasswordHelper.java
@@ -0,0 +1,43 @@
+package com.storehouse.business.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordHelper implements PasswordEncoder {
+
+	MessageDigest md;
+	
+	public PasswordHelper() {
+		try {
+			md=MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String encode(CharSequence rawPassword) {
+		if (md == null) {
+			return rawPassword.toString();
+		}
+		md.update(rawPassword.toString().getBytes());
+		byte byteData[] = md.digest();
+		StringBuffer hexString = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			String hex = Integer.toHexString(0xff & byteData[i]);
+			if (hex.length() == 1) {
+				hexString.append("0");
+			}
+			hexString.append(hex);
+		}
+		return hexString.toString();
+	}
+
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return encode(rawPassword).equals(encodedPassword);
+	}
+
+}
