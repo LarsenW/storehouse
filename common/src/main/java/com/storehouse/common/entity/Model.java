37a24abac99e9bbc14d088a6808a221f5cc6f5c8
LarsenW <wolflarsen10@gmail.com>
2015-11-17 13:15:43 +0200

    add create,update fields to Model class

--- common/src/main/java/com/storehouse/common/entity/Model.java
+++ common/src/main/java/com/storehouse/common/entity/Model.java
@@ -1,12 +1,21 @@
 package com.storehouse.common.entity;
 
+import java.util.Date;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 @MappedSuperclass
 public abstract class Model {
+
+	private Date created;
+
+	private Date updated;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -18,5 +27,14 @@ public abstract class Model {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	@PrePersist
+	protected void onCreate() {
+		created = new Date();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		updated = new Date();
+	}
 }
