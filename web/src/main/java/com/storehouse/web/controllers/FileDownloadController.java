96af0a855b6554153c81fe08bb67392090d6abae
LarsenW <wolflarsen10@gmail.com>
2015-12-04 16:58:37 +0200

    provide simple downloading

---
+++ web/src/main/java/com/storehouse/web/controllers/FileDownloadController.java
@@ -0,0 +1,47 @@
+package com.storehouse.web.controllers;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.storehouse.business.services.ItemService;
+import com.storehouse.common.entity.Item;
+
+@Controller
+public class FileDownloadController {
+	@Autowired
+	ItemService itemService;
+
+	@RequestMapping(value = "/download/{id}", method = RequestMethod.GET)
+	public void handleFileDownloading(@PathVariable Long id, HttpServletResponse response) {
+		
+		response.setContentType("application/octet-stream");
+		
+		Item item = itemService.getById(id);
+		byte[] data = item.getData();
+		
+		InputStream in = new ByteArrayInputStream(data);
+		try {
+			ServletOutputStream out = response.getOutputStream();
+			while (in.read(data, 0, data.length) != -1) {
+				out.write(data, 0, data.length);
+			}
+			in.close();
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
