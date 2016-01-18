50c6482d80efa0ad34d6c1c5411ea247b64c57e0
LarsenW <wolflarsen10@gmail.com>
2016-01-19 00:58:47 +0200

    refactor pom.xml,web/pom.xml

--- web/src/main/java/com/storehouse/web/controllers/ItemUploadController.java
+++ web/src/main/java/com/storehouse/web/controllers/ItemUploadController.java
@@ -2,13 +2,6 @@ package com.storehouse.web.controllers;
 
 import java.util.Calendar;
 
-import javax.jcr.Credentials;
-import javax.jcr.Node;
-import javax.jcr.Repository;
-import javax.jcr.Session;
-import javax.jcr.SimpleCredentials;
-
-import org.apache.jackrabbit.commons.JcrUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.core.Authentication;
@@ -42,29 +35,7 @@ public class ItemUploadController {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User user = (User) authentication.getPrincipal();
 		itemDto.setUser(user);
-
-		if (!file.isEmpty()) {
-			try {
-				Repository repository = JcrUtils.getRepository("http://localhost:2627/rmi");
-				Credentials credentials = new SimpleCredentials("admin", "admin".toCharArray());
-				Session session = repository.login(credentials);
-				Node node1 = (Node) session.getItem("/dept");
-				Node node2 = node1.addNode("folder", "nt:file");
-				Node content = node2.addNode("jcr:content", "nt:resource");
-				// content.setProperty("jcr:mimeType", "rtf");
-				content.setProperty("jcr:lastModified", Calendar.getInstance());
-				content.setProperty("jcr:data", session.getValueFactory().createBinary(file.getInputStream()));
-				// content.getSession().getValueFactory().createBinary(fis);
-				session.save();
-				System.out.println(file.getBytes().length);
-				itemCreatingService.createItem(itemDto);
-				return "profile";
-			} catch (Exception e) {
-				return "upload";
-			}
-		} else {
-			return "upload";
-		}
+		return "profile";
 	}
 
 }
--- web/src/main/java/com/storehouse/web/controllers/RegistrationController.java
+++ web/src/main/java/com/storehouse/web/controllers/RegistrationController.java
@@ -1,12 +1,17 @@
 package com.storehouse.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import com.storehouse.business.services.UserService;
 import com.storehouse.common.entity.User;
